ed36392fed876303effaf709a3eb91eed504ea4d
AndersonHJB <bornforthis@bornforthis.cn>
2025-09-24 18:21:01 +0800

    新增汽车生产成本和时间计算的基础代码结构

---
+++ bornforthis.cn/1v1/100-Leo/03/cost.py
@@ -0,0 +1,32 @@
+def main():
+    # The price of the car parts
+    car_door_cost: float = 55.5
+    car_wheel_cost: int = 20
+    tire_cost: float = 15.3
+    set_of_lights_cost: float = 10.16
+    engine_cost: int = 150
+    steering_wheel_cost: int = 40
+
+    prompt: str = "Enter the number of cars: "
+
+    # Task 1.1 Get the number of cars to produce/manufacture
+    # --- TODO below ---
+
+
+    # --- TODO above ---
+
+    # Task 1.2 Calculate the total cost
+    # --- TODO below ---
+
+
+    # --- TODO above ---
+
+    # Task 1.3 Print the total cost of car production
+    # Feel free to explore different print methods!
+    # --- TODO below ---
+
+
+    # --- TODO above ---
+
+if __name__ == "__main__":
+    main()
---
+++ bornforthis.cn/1v1/100-Leo/03/time_taken.py
@@ -0,0 +1,32 @@
+def main():
+    # The time taken to assemble of the car parts
+    car_door_time: int = 8
+    car_wheel_time: float = 5.8
+    tire_time: int = 5
+    set_of_lights_time: float = 10.5
+    engine_time: int = 20
+    steering_wheel_time: float = 12.7
+
+    prompt: str = "Enter the number of cars: "
+
+    # Task 2.1 Get the number of cars to produce/manufacture
+    # --- TODO below ---
+
+
+    # --- TODO above ---
+
+    # Task 2.2 Calculate the total time
+    # --- TODO below ---
+
+
+    # --- TODO above ---
+
+    # Task 2.3 Print the total time of car production
+    # Feel free to explore different print methods!
+    # --- TODO below ---
+
+
+    # --- TODO above ---
+
+if __name__ == "__main__":
+    main()
