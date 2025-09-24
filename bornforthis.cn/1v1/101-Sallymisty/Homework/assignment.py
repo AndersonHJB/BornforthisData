e071d1258e2b2aac1343806ec16ad346633158c1
AndersonHJB <bornforthis@bornforthis.cn>
2025-09-24 16:56:29 +0800

    Add initial temperature data for years 1975 to 1990 in test_temp_data.txt

---
+++ bornforthis.cn/1v1/101-Sallymisty/Homework/assignment.py
@@ -0,0 +1,42 @@
+### COMP1730/6730 project assignment
+
+"""
+ANU ID: u8338485
+NAME: Chengze Xin
+I declare that this submission is my own work
+https://www.anu.edu.au/students/academic-skills/academic-integrity
+"""
+
+def load_co2_data(filename):
+    pass
+
+def calculate_basic_statistics(co2_data, start_year, end_year):
+    pass
+
+def to_decimal_date(year, month_of_year, day_of_month):
+    pass
+
+def estimate_linear_trend(co2_data, start_year, end_year):
+    pass
+
+def extract_seasonal_component(co2_data, start_year, end_year):
+    pass
+
+def predict_co2_future_linear(co2_data, start_year, end_year, target_year, target_month, target_day):
+    pass
+
+def predict_co2_future_linear_and_seasonal(co2_data, start_year, end_year, target_year, target_month, target_day):
+    pass
+
+def load_temperature_data(filename):
+    pass
+
+def compute_correlation_co2_temperature(co2_data, temperature_data, start_year, end_year):
+    pass
+
+# This function is for COMP6730 only!
+# No need to attempt if you are a COMP1730 student
+def plot_data(co2_data, temperature_data):
+    pass
+
+
